b366245f370c85d6ffcf51a3c9d2e788ffa4d1b7
Mario Tsatsev <mario.tsatsev@student.ru.nl>
2020-05-26 21:14:31 +0200

    firstCommit

---
+++ cmake-build-debug/CMakeFiles/parallel_computing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parallel_computing.dir/main.c.o"
+  "parallel_computing"
+  "parallel_computing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/parallel_computing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
